a6bb85869ff20a926fdc2462ffd380f4b009f89c
Reinaldo Barifouse <seaderalst@gmail.com>
2022-03-28 17:03:22 -0300

    primeiro commit

---
+++ um teste/um teste/Script1.js
@@ -0,0 +1,13 @@
+let primeiroNumero = new.Array;
+
+let segundoNumero = 5;
+let terceiroNumero = 7;
+let umaFrase = "pague aqui";
+const mediaLocal = (segundoNumero + terceiroNumero) / 2;
+
+console.log( mediaLocal );
+
+
+function calcularQualquerCoisa () {
+
+}// JavaScript source code
